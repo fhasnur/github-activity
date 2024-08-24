2a363116edb86cb58866e39049ebcc5d2079b48e
fhasnur <fandi.hasnur10@gmail.com>
2024-08-24 08:41:15 +0700

    feat: basic CLI structure with cobra

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("Github Activity")
+	var rootCmd = &cobra.Command{
+		Use:   "github-activity [username]",
+		Short: "CLI to display user GitHub activity",
+		Run: func(cmd *cobra.Command, args []string) {
+			username := args[0]
+			fmt.Println(username)
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
